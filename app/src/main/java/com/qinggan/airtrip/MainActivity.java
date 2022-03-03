14f255ad4e167e43af9121c1cb6aa600ea030345
Jove Yu <yushijun110@126.com>
2022-03-03 12:58:04 +0800

    first

---
+++ app/src/main/java/com/qinggan/airtrip/MainActivity.java
@@ -0,0 +1,171 @@
+package com.qinggan.airtrip;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        ListView userInstalledApps = (ListView) findViewById(R.id.installed_app_list);
+
+        List<AppList> installedApps = getInstalledApps();
+        AppAdapter installedAppAdapter = new AppAdapter(MainActivity.this, installedApps);
+        userInstalledApps.setAdapter(installedAppAdapter);
+        userInstalledApps.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
+                Intent intent = getPackageManager().getLaunchIntentForPackage(installedApps.get(i).packages);
+                if(intent != null){
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+        userInstalledApps.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int i, long l) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(Uri.parse("package:" + installedApps.get(i).packages));
+                startActivity(intent);
+                return false;
+            }
+        });
+    }
+
+
+    private List<AppList> getInstalledApps() {
+        PackageManager pm = getPackageManager();
+        List<AppList> apps = new ArrayList<AppList>();
+        List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
+        //List<PackageInfo> packs = getPackageManager().getInstalledPackages(PackageManager.GET_PERMISSIONS);
+        for (int i = 0; i < packs.size(); i++) {
+            PackageInfo p = packs.get(i);
+            if ((!isSystemPackage(p))) {
+                String appName = p.applicationInfo.loadLabel(getPackageManager()).toString();
+                Drawable icon = p.applicationInfo.loadIcon(getPackageManager());
+                String packages = p.applicationInfo.packageName;
+                apps.add(new AppList(appName, icon, packages));
+            }
+        }
+        return apps;
+    }
+
+    private boolean isSystemPackage(PackageInfo pkgInfo) {
+        if (pkgInfo.applicationInfo.packageName.equals("com.qinggan.airtrip")) return true;
+        return (pkgInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
+    }
+
+    public class AppAdapter extends BaseAdapter {
+
+        public LayoutInflater layoutInflater;
+        public List<AppList> listStorage;
+
+        public AppAdapter(Context context, List<AppList> customizedListView) {
+            layoutInflater =(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            listStorage = customizedListView;
+        }
+
+        @Override
+        public int getCount() {
+            return listStorage.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder listViewHolder;
+            if(convertView == null){
+                listViewHolder = new ViewHolder();
+                convertView = layoutInflater.inflate(R.layout.installed_app_list, parent, false);
+
+                listViewHolder.textInListView = (TextView)convertView.findViewById(R.id.list_app_name);
+                listViewHolder.imageInListView = (ImageView)convertView.findViewById(R.id.app_icon);
+                listViewHolder.packageInListView=(TextView)convertView.findViewById(R.id.app_package);
+                convertView.setTag(listViewHolder);
+            }else{
+                listViewHolder = (ViewHolder)convertView.getTag();
+            }
+            listViewHolder.textInListView.setText(listStorage.get(position).getName());
+            listViewHolder.imageInListView.setImageDrawable(listStorage.get(position).getIcon());
+            listViewHolder.packageInListView.setText(listStorage.get(position).getPackages());
+
+            return convertView;
+        }
+
+        class ViewHolder{
+            TextView textInListView;
+            ImageView imageInListView;
+            TextView packageInListView;
+        }
+    }
+
+    public class AppList {
+        private String name;
+        Drawable icon;
+        private String packages;
+        public AppList(String name, Drawable icon, String packages) {
+            this.name = name;
+            this.icon = icon;
+            this.packages = packages;
+        }
+        public String getName() {
+            return name;
+        }
+        public Drawable getIcon() {
+            return icon;
+        }
+        public String getPackages() {
+            return packages;
+        }
+
+    }
+}
